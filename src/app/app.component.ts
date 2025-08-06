4772415b3e7c64a83a3032411a7694fda88a5685
nick1024working <nick1024working@gmail.com>
2025-08-06 09:13:37 +0800

    feat: 公用版型 > 新增 heroCarousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderWrapComponent } from "./shared/ui/header-wrap/header-wrap.component";
 import { FooterWrapComponent } from "./shared/ui/footer-wrap/footer-wrap.component";
+import { HeroCarouselComponent } from "./shared/ui/hero-carousel/hero-carousel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderWrapComponent, FooterWrapComponent],
+  imports: [RouterOutlet, HeaderWrapComponent, FooterWrapComponent, HeroCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/ui/hero-carousel/hero-carousel.component.ts
@@ -0,0 +1,19 @@
+// hero-carousel.component.ts
+import { Component } from '@angular/core';
+
+interface Banner {
+  title: string;
+  description: string;
+  image: string;
+}
+
+@Component({
+  selector: 'app-hero-carousel',
+  templateUrl: './hero-carousel.component.html',
+  styleUrls: ['./hero-carousel.component.css'],
+  standalone: true,
+  imports: []
+})
+export class HeroCarouselComponent {
+
+}
