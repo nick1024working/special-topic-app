b1cc89d41ca53bd6bdf71e6cdb4a6b3545b4d042
nick1024working <nick1024working@gmail.com>
2025-08-10 11:12:25 +0800

    temp

--- src/app/features/used-book/used-book.routes.ts
+++ src/app/features/used-book/used-book.routes.ts
@@ -1,14 +1,14 @@
-import { PublicBookDetailComponent } from './components/public-book-detail/public-book-detail.component';
 import { PublicBookListPageComponent } from './pages/public-book-list-page/public-book-list-page.component';
 import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
 import { CreateUsedBookPageComponent } from './pages/create-used-book-page/create-used-book-page.component';
 import { PlaceholderPageComponent } from './pages/placeholder-page/placeholder-page.component';
 import { TestPageComponent } from './pages/test-page/test-page.component';
+import { PublicBookDetailPageComponent } from './pages/public-book-detail-page/public-book-detail-page.component';
 import { Routes } from '@angular/router';
 
 export const USED_BOOK_ROUTES: Routes = [
     { path: '', component: PublicBookListPageComponent },
-    { path: 'books/:id', component: PublicBookDetailComponent },
+    { path: 'books/:id', component: PublicBookDetailPageComponent },
     { path: 'new', component: CreateUsedBookPageComponent },
     { path: 'products', component: ProductListPageComponent },
     {
