f28275550244097c5888a57b19aa7e3490ec8954
Simon <namomaahasitatapatreh@outlook.com>
2025-08-12 15:22:27 +0800

    2個dto

--- src/app/features/ebook/DTOs/ebook-summary.dto.ts
+++ src/app/features/ebook/DTOs/ebook-summary.dto.ts
@@ -1,6 +1,8 @@
+// 這個 interface 描述了書籍摘要的資料結構
 export interface EBookSummaryDto {
     ebookId: number;
     ebookName: string;
     author: string;
-    primaryCoverPath: string;
+    fixedPrice: number;
+    primaryCoverPath: string | null;
 }
