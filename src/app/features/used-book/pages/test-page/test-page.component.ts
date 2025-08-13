8eb0fd47e93fa5977d51aeb0b0009f282c876954
nick1024working <nick1024working@gmail.com>
2025-08-13 10:28:18 +0800

    feat: admin版型告一段落

--- src/app/features/used-book/pages/test-page/test-page.component.ts
+++ src/app/features/used-book/pages/test-page/test-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BookRowComponent } from "../../components/book-row/book-row.component";
 import { TestComponent } from "../../components/test/test.component";
+import { CreateUsedBookPageComponent } from "../create-used-book-page/create-used-book-page.component";
 
 @Component({
   selector: 'app-ub-test-page',
   standalone: true,
-  imports: [BookRowComponent, TestComponent],
+  imports: [BookRowComponent, TestComponent, CreateUsedBookPageComponent],
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.css'
 })
