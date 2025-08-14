90376592b44ee5cdec4596a0e8bf83099e9caf06
wade96307 <sunnyleo96307@gmail.com>
2025-08-14 14:57:44 +0800

    會員登入頁製作完成+可登入+驗證+雜湊密碼完成

---
+++ src/app/features/member/member-signup/member-signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-member-signup',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './member-signup.component.html',
+  styleUrl: './member-signup.component.css'
+})
+export class MemberSignupComponent {
+
+}
