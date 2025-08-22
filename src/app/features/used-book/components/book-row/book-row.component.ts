995e02392afbdc989663ba2cd1c6fb92130f4311
nick1024working <nick1024working@gmail.com>
2025-08-22 10:15:14 +0800

    feat: 修正 book-detail

--- src/app/features/used-book/components/book-row/book-row.component.ts
+++ src/app/features/used-book/components/book-row/book-row.component.ts
@@ -1,8 +1,6 @@
 import { Component, inject, Input } from '@angular/core';
 import { BookCardComponent } from "../book-card/book-card.component";
-import { BookListQuery, DEFAULT_BOOK_LIST_QUERY } from './../../dtos/book-list-query.dto';
 import { UsedBookService } from '../../services/used-book.service';
-import { PublicBookListItemDto } from '../../dtos/public-book-list-item.dto';
 import { BookCard } from '../../models/book-card.mode';
 
 @Component({
@@ -12,11 +10,6 @@ import { BookCard } from '../../models/book-card.mode';
     templateUrl: './book-row.component.html',
     styleUrl: './book-row.component.css'
 })
-
-/** 顯示 6 個 BookCard 的元件
- *
- *
- */
 export class BookRowComponent {
     private readonly _svc = inject(UsedBookService);
 
