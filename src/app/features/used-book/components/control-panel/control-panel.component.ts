50c046a4cf59a8e94a85da46f6bd11684e38637b
nick1024working <nick1024working@gmail.com>
2025-08-27 07:56:27 +0800

    update: ControlPanelComponent > 修改樣式與行為

--- src/app/features/used-book/components/control-panel/control-panel.component.ts
+++ src/app/features/used-book/components/control-panel/control-panel.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-ub-control-panel',
-  standalone: true,
-  imports: [],
-  templateUrl: './control-panel.component.html',
-  styleUrl: './control-panel.component.css'
+    selector: 'app-ub-control-panel',
+    standalone: true,
+    imports: [],
+    templateUrl: './control-panel.component.html',
+    styleUrls: ['./control-panel.component.css', '../../styles/bs-custom-override.scss',]
 })
 export class ControlPanelComponent {
+    isOpen = signal(false);
 
+    togglePanel(btn: HTMLButtonElement) {
+        this.isOpen.update(v => !v);
+        btn.blur();
+    }
 }
