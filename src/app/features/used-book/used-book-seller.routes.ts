6134c02314ab82c75fafc13a87d587c5f6a393af
nick1024working <nick1024working@gmail.com>
2025-09-04 13:15:07 +0800

    update: 更新時間顯示

--- src/app/features/used-book/used-book-seller.routes.ts
+++ src/app/features/used-book/used-book-seller.routes.ts
@@ -3,6 +3,7 @@ import { SellerBookListPageComponent } from './pages/seller-book-list-page/selle
 import { UserOrderListPageComponent } from './pages/user-order-list-page/user-order-list-page.component';
 import { CreateUsedBookPageComponent } from './pages/create-used-book-page/create-used-book-page.component';
 import { EditUsedBookPageComponent } from './pages/edit-used-book-page/edit-used-book-page.component';
+import { UserOrderDetailPageComponent } from './pages/user-order-detail-page/user-order-detail-page.component';
 
 export const USED_BOOK_SELLER_ROUTES: Routes = [
     { path: '', redirectTo: 'books', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const USED_BOOK_SELLER_ROUTES: Routes = [
     { path: 'books/new', component: CreateUsedBookPageComponent, title: "新增書本 | ProBookLand 賣家中心" },
     { path: 'books/edit/:id', component: EditUsedBookPageComponent, title: "編輯書本 | ProBookLand 賣家中心" },
     { path: 'orders', component: UserOrderListPageComponent, title: "訂單管理 | ProBookLand 賣家中心" },
+    { path: 'orders/:orderNo', component: UserOrderDetailPageComponent, title: "訂單詳情 | ProBookLand 賣家中心" },
 ];
